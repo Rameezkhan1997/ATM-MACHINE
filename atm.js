a4fa6aca1731e4acba9597bd733102b2e3c8f892
Ahmad Rameez Khan <160261538+Rameezkhan1997@users.noreply.github.com>
2024-05-23 16:53:40 +0300

    created atm project

---
+++ atm.js
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mybal = 11000;
+let myPin = 5211;
+console.log("Welcome to ATM Machine");
+let PinAns = await inquirer.prompt([
+    {
+        name: "Pin",
+        type: "number",
+        message: "Enter Your Pin code:"
+    }
+]);
+if (PinAns.Pin === myPin) {
+    console.log("Login Successfully!");
+    let OperationAns = await inquirer.prompt([
+        {
+            name: "Operation",
+            type: "list",
+            message: "Select an Operation",
+            choices: ["Withdraw", "Balance Inquiry"]
+        }
+    ]);
+    if (OperationAns.Operation === "Withdraw") {
+        let AmountAns = await inquirer.prompt([
+            {
+                name: "Amount",
+                type: "number",
+                message: "Enter Your Amount:"
+            }
+        ]);
+        if (AmountAns.Amount > mybal) {
+            console.log("Insufficient Balance");
+        }
+        else {
+            mybal -= AmountAns.Amount;
+            console.log(`${AmountAns.Amount} Withdrawn successfully`);
+            console.log(`Your Current Balance is: ${mybal}`);
+        }
+    }
+    else if (OperationAns.Operation === "Balance Inquiry") {
+        console.log(`Your Current Balance is: ${mybal}`);
+    }
+}
+else {
+    console.log("Your Pin is incorrect");
+}
